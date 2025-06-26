ab945533a166da646fb96503b07acd97db155ea5
samheelangstrom <samuel.heel@angstromsports.com>
2025-06-26 09:49:01 +0100

    Align with project structure

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const morgan = require("morgan");
+const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use("/", routes);
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
---
+++ src/services/mysqlService.js
@@ -0,0 +1,2 @@
+// TODO: Add MySQL connection utilities.
+
